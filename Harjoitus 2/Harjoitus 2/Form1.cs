7b3af35ca5418e78f117de3c6f88c221c0e6bee1
Haamulaine <eemeli.ea.laine@gmail.com>
2026-02-23 13:25:10 +0200

    Harjoitus 2

---
+++ Harjoitus 2/Harjoitus 2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_2
+{
+    public partial class Harjoitus2 : Form
+    {
+        public Harjoitus2()
+        {
+            InitializeComponent();
+        }
+
+        private void TulostaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = ViestiTB.Text;
+            TulostusLB.Text = teksti;
+            TulostusLB.Visible = true;
+        }
+    }
+}
